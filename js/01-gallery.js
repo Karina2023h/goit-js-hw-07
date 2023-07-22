ef1e1f19b97f1b62324d2fca57c36bceac2995a6
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2023-07-22 21:41:46 +0200

    js7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const itemsGalleryCreat = createGalleryItems(galleryItems);
+
+gallery.addEventListener("click", handleClick);
+gallery.insertAdjacentHTML("beforeend", itemsGalleryCreat);
+
+function createGalleryItems(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+const instance = basicLightbox.create(`<img src="" width="800" height="600">`, {
+  onShow: (instance) => {
+    document.addEventListener("keydown", logMessage);
+  },
+  onClose: (instance) => {
+    document.removeEventListener("keydown", logMessage);
+  },
+});
+
+function handleClick(a) {
+  a.preventDefault();
+  const datasetSource = a.target.dataset.source;
+  instance.element().querySelector("img").src = datasetSource;
+  instance.show();
+}
+
+function logMessage(a) {
+  if (a.code === "Escape");
+  instance.close();
+}
